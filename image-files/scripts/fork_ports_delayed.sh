9552efe1148d7189d28bf04bc6085430dcd64466
Nich <nich@nixnet.com>
2023-06-10 15:12:53 +0100

    fix var in display forking

--- image-files/scripts/fork_ports_delayed.sh
+++ image-files/scripts/fork_ports_delayed.sh
@@ -2,12 +2,10 @@
 
 sleep 30
 
-intport = ${IBC_OverrideTwsApiPort:-4000}
-
+intport=${IBC_OverrideTwsApiPort:-4000}
+extport = ${TWS_API_PORT:-4001}
 if [ "$TRADING_MODE" = "paper" ]; then
-  extport = ${TWS_API_PORT:-4002}
-else
-  extport = ${TWS_API_PORT:-4001}
+  extport=${TWS_API_PORT:-4002}
 fi
 
 printf "Forking :::${intport} onto 0.0.0.0:${extport}\n"
