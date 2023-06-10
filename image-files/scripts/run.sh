64cd9802a6cfc44b938acb75e3afb1821c77eb91
Nich <nich@nixnet.com>
2023-06-10 14:26:47 +0100

    Fix run.sh

--- image-files/scripts/run.sh
+++ image-files/scripts/run.sh
@@ -19,14 +19,13 @@ echo /root/scripts/replace.sh ${IBC_INI}
 
 /root/scripts/fork_ports_delayed.sh &
 
-gatewayparam =
-if [[ -z ${TWS_OR_GATEWAY} ]]; then
+if [ -z ${TWS_OR_GATEWAY} ]; then
   gatewayparam = "-g"
 else
   gatewayparam = 
 fi
 
-/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" ${gatewayparam} \
+/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" $gatewayparam \
      "--tws-path=${TWS_PATH}" \
      "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
      "--user=${TWS_USERID}" "--pw=${TWS_PASSWORD}" "--mode=${TRADING_MODE}" \
