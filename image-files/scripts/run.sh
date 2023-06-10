160257a7213deeff0953dd60234a4de83feda1a4
Nich <nich@nixnet.com>
2023-06-10 14:20:21 +0100

    fix run issues

--- image-files/scripts/run.sh
+++ image-files/scripts/run.sh
@@ -10,13 +10,16 @@ if [ -n "$VNC_SERVER_PASSWORD" ]; then
   /root/scripts/run_x11_vnc.sh &
 fi
 
+printf "copying IBC config to %s" ${IBC_INI}
 envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
 
 # Override IBC_INI with environment variables
-./replace.sh ${IBC_INI}
+echo /root/scripts/replace.sh ${IBC_INI}
+/root/scripts/replace.sh ${IBC_INI}
 
 /root/scripts/fork_ports_delayed.sh &
 
+gatewayparam =
 if [[ -z ${TWS_OR_GATEWAY} ]]; then
   gatewayparam = "-g"
 else
