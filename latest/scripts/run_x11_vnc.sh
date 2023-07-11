547c6c8a0987115519efcba457ea68e59b645565
Nich Overend <nich@oticapital.co.uk>
2023-07-11 10:16:45 +0000

    Changes for working system

--- latest/scripts/run_x11_vnc.sh
+++ latest/scripts/run_x11_vnc.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-x11vnc -ncache_cr -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6 -passwd "$VNC_SERVER_PASSWORD"
+x11vnc -ncache_cr -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6 -passwd "$VNC_SERVER_PASSWORD" -rfbport ${VNC_SERVER_PORT:-5900}
+
+# Start noVNC server
+./noVNC/utils/novnc_proxy --vnc localhost:${VNC_SERVER_PORT:-5900} --listen 0.0.0.0:${NOVNC_SERVER_PORT} &
--- stable/scripts/run.sh
+++ stable/scripts/run.sh
@@ -3,18 +3,30 @@
 export DISPLAY=:1
 
 rm -f /tmp/.X1-lock
-Xvfb :1 -ac -screen 0 1024x768x16 &
+Xvfb :1 -ac -screen 0 1920x1200x16 &
 
 if [ -n "$VNC_SERVER_PASSWORD" ]; then
   echo "Starting VNC server"
   /root/scripts/run_x11_vnc.sh &
 fi
 
+printf "copying IBC config to %s" ${IBC_INI}
 envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
+
+# Override IBC_INI with environment variables
+echo /root/scripts/replace.sh ${IBC_INI}
+/root/scripts/replace.sh ${IBC_INI}
 
 /root/scripts/fork_ports_delayed.sh &
 
-/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" -g \
+gatewayparam=""
+if [ -z ${TWS_OR_GATEWAY} ]; then
+  gatewayparam="-g"
+elif [ ${TWS_OR_GATEWAY} = "gateway" ]; then
+  gatewayparam="-g"
+fi
+
+/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" $gatewayparam \
      "--tws-path=${TWS_PATH}" \
      "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
      "--user=${TWS_USERID}" "--pw=${TWS_PASSWORD}" "--mode=${TRADING_MODE}" \
